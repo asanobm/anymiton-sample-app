81f7bffde11a17889809ebd4207816ed7a6e46fc
Asanobm <kim_bumin@fourglobe.co.jp>
2020-10-07 23:08:07 +0900

    WIP: video

--- src/pages/ConfigPage.tsx
+++ src/pages/ConfigPage.tsx
@@ -1,10 +1,12 @@
 import { Layout } from 'antd'
 import React from 'react'
+import Recoder from '../components/Recoder'
 
 const ConfigPage: React.FC = () => {
   return (
     <Layout>
       <h1>Config Page</h1>
+      <Recoder />
     </Layout>
   )
 }
