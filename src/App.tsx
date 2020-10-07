3005863829170f6344e7997194a6af99fcc3cd1b
Asanobm <kim_bumin@fourglobe.co.jp>
2020-10-07 10:20:53 +0900

    WIP: add dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
+import { Layout, Spin } from 'antd'
 import React from 'react'
-import './App.css'
-import Movies from './components/Movies'
+import Header from './components/Header'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import ConfigPage from './pages/ConfigPage'
+import useIsLoading from './hooks/useIsLoading'
 
 const App: React.FC = () => {
+  const { isLoading } = useIsLoading()
   return (
-    <div className="App">
-      <Movies />
-    </div>
+    <Layout>
+      {isLoading ? (
+        <Spin tip="Loading..."></Spin>
+      ) : (
+        <Router>
+          <Header />
+          <Layout>
+            <Route exact path="/" component={HomePage}></Route>
+            <Route path="/config" component={ConfigPage}></Route>
+          </Layout>
+        </Router>
+      )}
+    </Layout>
   )
 }
 
