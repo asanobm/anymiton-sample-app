d93fe95c11bd38a257f4c1c6bf969398a60e0bd0
Asanobm <kim_bumin@fourglobe.co.jp>
2020-10-07 10:17:37 +0900

    add: ConfigPage component

---
+++ src/pages/ConfigPage.tsx
@@ -0,0 +1,12 @@
+import { Layout } from 'antd'
+import React from 'react'
+
+const ConfigPage: React.FC = () => {
+  return (
+    <Layout>
+      <h1>Config Page</h1>
+    </Layout>
+  )
+}
+
+export default ConfigPage
