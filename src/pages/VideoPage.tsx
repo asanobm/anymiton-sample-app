1b716ce115fe4bb6f62d02a8568c980bbbfac011
Asanobm <kim_bumin@fourglobe.co.jp>
2020-10-09 13:19:27 +0900

    update: video page

--- src/pages/VideoPage.tsx
+++ src/pages/VideoPage.tsx
@@ -2,20 +2,41 @@ import React from 'react'
 import { Layout, Space, Typography } from 'antd'
 import { useParams } from 'react-router-dom'
 import { Video } from '../interfaces/Video'
-
+import * as H from 'history'
 import ReactPlayer from 'react-player'
 import { API } from '../config/Api'
 import VideoPlayer from '../components/VideoPlayer'
 
 const { Title } = Typography
 
-const VideoPage: React.FC = () => {
+interface Props {
+  history: H.History
+}
+
+const VideoPage = (props: Props) => {
+  console.log(props.history)
   const { title } = useParams<Video>()
-  console.log(title)
+
+  const handleEndedVideo = () => {
+    props.history.push(`/record/${title}`)
+  }
+
   return (
-    <Layout style={{ backgroundColor: 'black', height: 'calc(100vh - 46px)' }}>
-      <Title>Teaching Video: {title}</Title>
-      <VideoPlayer title={title} />
+    <Layout
+      style={{
+        backgroundColor: 'black',
+        height: 'calc(100vh - 46px)',
+        justifyContent: 'center',
+        display: 'flex',
+        flexDirection: 'row',
+      }}
+    >
+      <VideoPlayer
+        height={window.outerHeight - window.outerHeight / 2}
+        title={title}
+        playing={true}
+        onEnded={handleEndedVideo}
+      />
     </Layout>
   )
 }
