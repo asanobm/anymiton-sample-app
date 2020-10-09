0c8cde7e2f432e064f6425b56a47d8f1ac1660d4
Asanobm <kim_bumin@fourglobe.co.jp>
2020-10-09 13:20:37 +0900

    remove

--- src/pages/ConfigPage.tsx
+++ src/pages/ConfigPage.tsx
@@ -1,12 +1,10 @@
 import { Layout } from 'antd'
 import React from 'react'
-import Recoder from '../components/Recoder'
 
 const ConfigPage: React.FC = () => {
   return (
     <Layout>
       <h1>Config Page</h1>
-      <Recoder />
     </Layout>
   )
 }
