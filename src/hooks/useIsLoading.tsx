0bd498798dd7d63bad809d5ea9a6d3a326f78738
Asanobm <kim_bumin@fourglobe.co.jp>
2020-10-09 13:17:41 +0900

    delete

--- src/hooks/useIsLoading.tsx
+++
@@ -1,9 +0,0 @@
-import { useState } from 'react'
-
-const useIsLoading = () => {
-  const [isLoading, setIsLoading] = useState<boolean>(false)
-
-  return { isLoading, setIsLoading }
-}
-
-export default useIsLoading
